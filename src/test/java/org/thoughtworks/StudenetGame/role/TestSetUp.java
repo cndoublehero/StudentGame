05fb1e76e196105f531281cfb815ab9bdee5561a
cndoublehero <cndoublehero@gmail.com>
2014-05-01 10:59:02 +0800

    reset GameNumber state on each setUp function

--- src/test/java/org/thoughtworks/StudenetGame/role/TestSetUp.java
+++ src/test/java/org/thoughtworks/StudenetGame/role/TestSetUp.java
@@ -5,6 +5,7 @@ import org.thoughtworks.StudenetGame.number.GameNumber;
 public class TestSetUp {
 
 	public static void setUp() {
+		GameNumber.resetGameNumber();
 		GameNumber.getAndSetFirstGameNumber(3);
 		GameNumber.getAndSetSecondGameNumber(5);
 		GameNumber.getAndSetThreeGameNumber(7);
